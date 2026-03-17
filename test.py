a606821c2e8c2fb5492558caa27d3dbb69c2337d
perumal <nallaperumal@gmail.com>
2026-03-17 15:18:00 +0530

    included welcome

--- test.py
+++ test.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+@app.route('/welcome')
+def Welcome():
+    return 'welcome to flsk'
+
 if __name__ == '__main__':
     app.run()
